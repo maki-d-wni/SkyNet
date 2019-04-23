bc69b908e1806eafdc669b460564293cb376e4e7
maki-d-wni <maki-d@wni.com>
2019-04-23 09:23:31 +0000

    tmp

--- datasets/make.py
+++ datasets/make.py
@@ -32,11 +32,10 @@ def check_grib(grbs, ft, param, level=None):
             return False
 
 
-def msm_airport_ft0(icaos):
+def msm_airport_ft0(icaos, save_dir):
     import re
     import glob
     import gc
-    import pickle
     import pygrib
     import skynet.nwp2d as npd
     from skynet import MSM_INFO, MSM_DATA_DIR
@@ -48,8 +47,8 @@ def msm_airport_ft0(icaos):
     tagid_list = [tagid for tagid in MSM_INFO.keys() if re.match(r'4002200', tagid)]
     tagid_list.sort()
 
-    df_airports = {icao: npd.NWPFrame() for icao in icaos}
     for icao in icaos:
+        msm_point = npd.NWPFrame()
         for tagid in tagid_list:
             meta = MSM_INFO[tagid]
 
@@ -81,7 +80,7 @@ def msm_airport_ft0(icaos):
 
                         lat = sf_latlon_idx[icao][0]
                         lon = sf_latlon_idx[icao][1]
-                        df_airports[icao].loc[date, param] = grb.values[lat, lon]
+                        msm_point.loc[date, param] = grb.values[lat, lon]
 
                         del grb
                         gc.collect()
@@ -95,15 +94,14 @@ def msm_airport_ft0(icaos):
 
                         lat = up_latlon_idx[icao][0]
                         lon = up_latlon_idx[icao][1]
-                        df_airports[icao].loc[date, param] = grb.values[lat, lon]
+                        msm_point.loc[date, param] = grb.values[lat, lon]
 
                         del grb
                         gc.collect()
 
                     grbs.close()
 
-        df_airports[icao].to_csv('/Users/makino/PycharmProjects/SkyCC/data/msm_airport/%s.csv' % icao)
-    pickle.dump(df_airports, open('/Users/makino/PycharmProjects/SkyCC/data/all_airport.pkl', 'wb'))
+        msm_point.to_csv('%s/%s.csv' % (save_dir, icao))
 
 
 def msm_airport_xy(icao, metar_dir, msm_dir, save_dir):
@@ -160,13 +158,15 @@ def msm_airport_xy(icao, metar_dir, msm_dir, save_dir):
 
 
 def main():
+    import os
+    import skynet.nwp2d as npd
     from skynet import DATA_DIR
 
-    # jp_icaos = msm.get_jp_icaos()
-    jp_icaos = [
+    jp_icaos = npd.msm.get_jp_icaos()
+    jp_icaos2 = [
         # 'RJOT',
-        # 'RJAA',
-        # 'RJBB',
+        'RJAA',
+        'RJBB',
         'RJCC',
         'RJCH',
         'RJFF',
@@ -186,14 +186,19 @@ def main():
         'RJOO',
     ]
 
-    # msm_airport_ft0(jp_icaos)
+    jp_icaos = [icao for icao in jp_icaos if not (icao in jp_icaos2)]
+    jp_icaos.sort()
+
+    save_dir = '%s/MSM/airport' % DATA_DIR
+    os.makedirs(save_dir, exist_ok=True)
+    msm_airport_ft0(jp_icaos, save_dir)
 
     icao = 'RJAA'
     metar_dir = '%s/metar/airport' % DATA_DIR
     msm_dir = '%s/MSM/airport' % DATA_DIR
     save_dir = '%s/MSM/airport.process' % DATA_DIR
 
-    msm_airport_xy(icao, metar_dir, msm_dir, save_dir)
+    # msm_airport_xy(icao, metar_dir, msm_dir, save_dir)
 
 
 if __name__ == '__main__':
